cffd1209146f9451fedf79edcc2c000ddd165430
Rujal <rujalshrsth@gmail.com>
2024-12-30 03:51:50 +0545

    sessioncontrol updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostCommentsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -8,15 +9,15 @@ use App\Http\Controllers\SessionsController;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
-Route::post('/')
+// Route::post('/posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
 
 Route::get('register',[RegisterController::class, 'create'])->middleware('guest');
 
 Route::post('register',[RegisterController::class, 'store'])->middleware('guest');
 
-Route::get('login',[SessionsController::class, 'create'])->middleware('guest');
-Route::post('session',[SessionsController::class, 'store'])->middleware('guest');
+Route::get('login',[SessionsController::class, 'create'])->middleware('guest')->name('login');
+Route::post('login',[SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout',[SessionsController::class, 'destroy'])->middleware('auth');
 
