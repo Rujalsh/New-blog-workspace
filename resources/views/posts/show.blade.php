6ca5d61be3fd67c634ed98242ba68743ab9673d3
Rujal <rujalshrsth@gmail.com>
2024-12-30 03:50:10 +0545

    comment form action updated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,33 +52,44 @@
                 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
 
+                @auth
+                    
                 <x-panel>
-
-                    <form action="POST" action="/" >
+                    
+                    <form method="POST" action="/posts/{{ $post->slug }}/comments" >
+                        
                         @csrf
-                    
+                        
                         <header class="flex items-center">
-                        <img src="https://i.pravatar.cc/60?u={{ Auth::id() }}" alt="" width="40" height="40" class="rounded-full">
-                        <h2 class="ml-4">
-                            Want to participate?
-                        </h2>
+                            <img src="https://i.pravatar.cc/60?u={{ Auth::id() }}" alt="" width="40" height="40" class="rounded-full">
+                            <h2 class="ml-4">
+                                Want to participate?
+                            </h2>
                         </header>
-
+                        
                         <div class="mt-6">
-                            <textarea name="body" id="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, think of something to say!"></textarea>
-                        </div>
-
-                        <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                            {{-- <x-submit-button> --}}
+                            <textarea 
+                            name="body" 
+                            class="w-full text-sm focus:outline-none focus:ring" 
+                            rows="5" 
+                            placeholder="Quick, think of something to say!"></textarea>
+                </div>
+                
+                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                    {{-- <x-submit-button> --}}
                         <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
-                                
+                            
                             Post
-                    
+                            
                         </button>
-                            {{-- </x-submit-button> --}}
-                        </div>
-                    </form>
-                </x-panel>
+                        {{-- </x-submit-button> --}}
+                    </div>
+                </form>
+            </x-panel>
+        @else
+            <p class="font-semibold">
+                <a href="/register" class="font-semibold hover:underline text-blue-500">Register</a> or <a href="/login" class="font-semibold hover:underline text-blue-500">Log in</a> to leave a comment.
+            @endauth
 
                     @foreach ($post ->comments as $comment)
                         <x-post-comment :comment="$comment" />
