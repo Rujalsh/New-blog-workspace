a25498aa1554479252f89695cd09cf722d5f9467
Rujal <rujalshrsth@gmail.com>
2025-01-04 11:59:47 +0545

    replacement of kernel.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Register your custom middleware
+        $middleware->web(append: [
+        \App\Http\Middleware\MustBeAdministrator::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
