fe4fd271dff0818dc0d8a122196906aef5d98288
Rujal <rujalshrsth@gmail.com>
2025-01-06 02:10:07 +0545

    simplifying routes for admin

--- routes/web.php
+++ routes/web.php
@@ -31,16 +31,18 @@ Route::post('logout',[SessionsController::class, 'destroy'])->middleware('auth')
 // });
 
 
-Route::middleware('admin')->group(function () {
-    
-    Route::post('admin/posts', [AdminPostController::class, 'store']);
-    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
+Route::middleware('can:admin')->group(function () {
+    Route::resource('admin/posts', AdminPostController::class)->except('show');
 
-    Route::get('admin/posts',[AdminPostController::class, 'index']);
-    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+    //*****Individual Route **********//
+    // Route::post('admin/posts', [AdminPostController::class, 'store']);
+    // Route::get('admin/posts/create', [AdminPostController::class, 'create']);
 
-    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
-    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destory']);
+    // Route::get('admin/posts',[AdminPostController::class, 'index']);
+    // Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+
+    // Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
+    // Route::delete('admin/posts/{post}', [AdminPostController::class, 'destory']);
 
 });
 //************ */ previous code********
