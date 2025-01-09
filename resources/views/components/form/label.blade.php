27d48fdb941b9388e2cefe6c7ed47ce20495a5cd
Rujal <rujalshrsth@gmail.com>
2025-01-09 16:18:48 +0545

    minor changes

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,7 +1,5 @@
 @props(['name'])
 
-<label for="{{$name
-
-}}" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+<label for="{!!$name!!}" class="block mb-2 uppercase font-bold text-xs text-gray-700">
     {{ucwords($name)}}
 </label>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,13 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.3/dist/cdn.min.js"></script>
+
+{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" defer></script> --}}
+
 
 <style>
     html {
