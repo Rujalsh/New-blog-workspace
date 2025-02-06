27a9969964fdda1c33e847d3d39f485d541ddba1
Rujal <rujalshrsth@gmail.com>
2025-02-06 13:47:16 +0545

    updating the navigation controller

---
+++ app/Http/Controllers/NavController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NavController extends Controller
+{
+    //
+    public function about()
+    {
+        return view('components.layouts.navbar.about-page');
+    }
+    public function showContactUs()
+    {
+        return view('components.layouts.navbar.contact-us');
+    }
+}
