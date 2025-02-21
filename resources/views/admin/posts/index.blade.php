2c59a13572038b0f41a6acf8e1ca7662b818763b
Rujal <rujalshrsth@gmail.com>
2025-02-22 00:48:17 +0545

    change in size and location to dashboad-master

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,44 +1,33 @@
-<x-layouts.layout>
+<x-dashboard.master heading="Manage Posts">
     <x-setting heading="Manage Posts">
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="py-6 align-middle inline-block min-w-full sm:px-10 lg:px-12"> <!-- Increased padding -->
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table class="min-w-full divide-y divide-gray-200">
-                            
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($posts as $post)
+                        <table class="min-w-full divide-y divide-gray-200"> <!-- Set larger minimum width -->
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach ($posts as $post)
                                 <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    <td class="px-12 py-10 whitespace-nowrap"> <!-- Further increased padding -->
                                         <div class="flex items-center">
-                                            <div class="text-sm font-medium text-gray-900">
+                                            <div class="text-4xl font-medium text-gray-900"> <!-- Increased font size to match h2 -->
                                                 <a href="/posts/{{ $post->slug }}">
                                                     {{ $post->title }}
                                                 </a>
-                                                </div>
                                             </div>
-                                        </td>
-
-                                        {{-- <td class="px-6 py-4 whitespace-nowrap ">
-                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                            Published
-                                        </span>
-                                    </td> --}}
-                                    
-
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <a href="/admin/posts/{{ $post->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <form method="POST" action="/admin/posts/{{ $post->id }}">
-                                                @csrf
-                                                @method('DELETE')
-
-                                                <button class="text-xs text-gray-400">Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
+                                        </div>
+                                    </td>
+                                    <td class="px-12 py-10 whitespace-nowrap text-right text-4xl font-medium"> <!-- Increased font size to match h2 -->
+                                        <a href="/admin/posts/{{ $post->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
+                                    </td>
+                                    <td class="px-12 py-10 whitespace-nowrap text-right text-4xl font-medium"> <!-- Increased font size to match h2 -->
+                                        <form method="POST" action="/admin/posts/{{ $post->id }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="text-3xl text-gray-400 hover:text-gray-600">Delete</button> <!-- Increased text size to match h2 -->
+                                        </form>
+                                    </td>
+                                </tr>
                                 @endforeach
                             </tbody>
                         </table>
@@ -47,4 +36,4 @@
             </div>
         </div>
     </x-setting>
-</x-layouts.layout>
+</x-dashboard.master>
