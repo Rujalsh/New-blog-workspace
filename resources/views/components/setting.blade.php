fb6f0d22ff90132a2c04ae143f9628cb80056819
Rujal <rujalshrsth@gmail.com>
2025-02-22 00:48:34 +0545

    removal css

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -1,12 +1,13 @@
 @props(['heading'])
+
 <section class=" py-8 max-w-4xl mx-auto">
 
-        <h1 class="text-xl font-bold mb-8 pb-2 border-b-4">
+        {{-- <h1 class="text-xl font-bold mb-8 pb-2 border-b-4">
             {{$heading}}
-        </h1>
+        </h1> --}}
 
     <div class="flex">
-        <aside class="bg-gray-100 border border-gray-200 p-4 rounded-xl mr-8 ">
+        {{-- <aside class="bg-gray-100 border border-gray-200 p-4 rounded-xl mr-8 ">
             <h4 class="font-bold mb-4">Links</h4>
             <ul>
                 <li> 
@@ -16,7 +17,7 @@
                     <a href="/admin/posts/create" class="{{request()->is('admin/posts/create') ? 'text-blue-500': ''}}">New Post</a>
                 </li>
             </ul>
-        </aside>
+        </aside> --}}
 
         <main class="flex-1">
             <x-panel>
@@ -25,3 +26,4 @@
         </main>
     </div>
 </section> 
+
